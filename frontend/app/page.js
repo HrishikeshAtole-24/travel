eb536f631f7c546b243c6db3e14575d18359533a
HrishikeshAtole-24 <rishiatole4545@gmail.com>
2026-01-04 05:44:44 +0530

    feat: Remove PopularFlights component from Home page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,6 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import PopularDestinations from './components/PopularDestinations/PopularDestinations';
-import PopularFlights from './components/PopularFlights/PopularFlights';
 import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs';
 import FAQ from './components/FAQ/FAQ';
 import Footer from './components/Footer/Footer';
@@ -13,7 +12,6 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <PopularFlights />
         <PopularDestinations />
         <WhyChooseUs />
         <FAQ />
